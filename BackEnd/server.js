6969934fa4ab164d1a7c7015e23a2924d03f2614
Sarah O'Connor <sarahaoconnor10@gmail.com>
2024-12-01 18:56:03 +0000

    Setting up cors, body parser, mongodb

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,2 +1,20 @@
 // set up server on port 4000
 // link to mongodb db
+
+
+const cors = require('cors');
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://admin:admin@cluster0.4a5pk.mongodb.net/DB11');
