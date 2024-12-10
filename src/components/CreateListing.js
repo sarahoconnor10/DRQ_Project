e20b927a18775db91d029aee5b8c03426f4b2176
Sarah O'Connor <sarahaoconnor10@gmail.com>
2024-12-10 14:31:29 +0000

    form to create new listing + submission handler

--- src/components/CreateListing.js
+++ src/components/CreateListing.js
@@ -4,9 +4,63 @@ import { useState } from "react";
 //form to input animal data + submit button
 
 const CreateListing = () => {
+    const [name, setName] = useState('');
+    const [animalType, setAnimalType] = useState('');
+    const [age, setAge] = useState('');
+    const [image, setImage] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const animal = { name, animalType, age, image };
+        console.log(animal);
+
+        axios.post('http://localhost:4000/api/Animals', animal)
+            .then((res) => { console.log(res.data) })
+            .catch();
+    }
+
     return (
-        <h1>CreateListing component</h1>
-    )
+        <div>
+            <h1>Create new listing</h1>
+            <form>
+                <div className="form-group">
+                    <label>name:</label>
+                    <input type="text"
+                        className="form-control"
+                        value={name}
+                        onChange={(e) => { setName(e.target.value) }}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Type:</label>
+                    <input type="text"
+                        className="form-control"
+                        value={animalType}
+                        onChange={(e) => { setAnimalType(e.target.value) }}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Age:</label>
+                    <input type="text"
+                        className="form-control"
+                        value={age}
+                        onChange={(e) => { setAge(e.target.value) }}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Image link:</label>
+                    <input type="text"
+                        className="form-control"
+                        value={image}
+                        onChange={(e) => { setImage(e.target.value) }}
+                    />
+                </div>
+                <div>
+                    <input type="submit" value="Create listing"></input>
+                </div>
+            </form>
+        </div>
+    );
 }
 
 export default CreateListing;
