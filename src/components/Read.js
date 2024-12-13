6bd545053a08d76a6427a4daea6be961c146a2e6
Sarah O'Connor <sarahaoconnor10@gmail.com>
2024-12-13 14:00:59 +0000

    Filter display by animal type

--- src/components/Read.js
+++ src/components/Read.js
@@ -4,12 +4,16 @@ import axios from "axios";
 
 const Read = () => {
     const [animals, setAnimals] = useState([]);
+    const [filteredAnimals, setFilteredAnimals] = useState([]);
+    const [animalType, setAnimalType] = useState("");
+
 
     const ReloadData = () => {
         axios.get('http://localhost:4000/api/Animals')
         .then((res) => {
             console.log(res.data);
             setAnimals(res.data.animals);
+            setFilteredAnimals(res.data.animals);
         })
         .catch((e) => {
             console.log(e);
@@ -20,9 +24,37 @@ const Read = () => {
         ReloadData();
     }, []);
 
+    const handleFilterChange = (e) => {
+        const selected = e.target.value;
+        setAnimalType(selected);
+
+        if(selected === ""){
+            setFilteredAnimals(animals); //show all
+        }
+        else {
+            const filtered = animals.filter(animal => animal.animalType === selected);
+            setFilteredAnimals(filtered);
+        }
+    }
+
     return (
         <div>
-            <AdoptableAnimals Animals={animals}  ReloadData={ReloadData}/>
+            <div style={{ margin: "1rem 0", textAlign: "center" }}>
+                <label htmlFor="filter" style={{ marginRight: "0.5rem" }}>Filter by Animal Type:</label>
+                <select
+                    id="filter"
+                    value={animalType}
+                    onChange={handleFilterChange}
+                    style={{ padding: "0.5rem", fontSize: "1rem" }}
+                >
+                    <option value="">All</option>
+                    <option value="Dog">Dog</option>
+                    <option value="Cat">Cat</option>
+                    <option value="Small Mammal">Small Mammal</option>
+                    <option value="Other">Other</option>
+                </select>
+            </div>
+            <AdoptableAnimals Animals={filteredAnimals}  ReloadData={ReloadData}/>
         </div>
     );
 }
